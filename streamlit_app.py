8fe23807668cb2206c601234d2fe74c395f39d98
yamada_ <139644652+TeruoYamada@users.noreply.github.com>
2025-04-10 13:37:33 -0400

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,66 @@
+import cdsapi
+
+dataset = "reanalysis-era5-single-levels"
+request = {
+    "product_type": [
+        "reanalysis",
+        "ensemble_members",
+        "ensemble_mean",
+        "ensemble_spread"
+    ],
+    "variable": [
+        "10m_u_component_of_wind",
+        "10m_v_component_of_wind",
+        "total_precipitation",
+        "100m_u_component_of_wind",
+        "100m_v_component_of_wind",
+        "10m_u_component_of_neutral_wind",
+        "10m_v_component_of_neutral_wind",
+        "10m_wind_gust_since_previous_post_processing",
+        "instantaneous_10m_wind_gust",
+        "convective_precipitation",
+        "convective_rain_rate",
+        "instantaneous_large_scale_surface_precipitation_fraction",
+        "large_scale_rain_rate",
+        "large_scale_precipitation",
+        "large_scale_precipitation_fraction",
+        "maximum_total_precipitation_rate_since_previous_post_processing",
+        "minimum_total_precipitation_rate_since_previous_post_processing",
+        "precipitation_type",
+        "total_column_rain_water"
+    ],
+    "year": ["2025"],
+    "month": [
+        "01", "02", "03",
+        "04"
+    ],
+    "day": [
+        "01", "02", "03",
+        "04", "05", "06",
+        "07", "08", "09",
+        "10", "11", "12",
+        "13", "14", "15",
+        "16", "17", "18",
+        "19", "20", "21",
+        "22", "23", "24",
+        "25", "26", "27",
+        "28", "29", "30",
+        "31"
+    ],
+    "time": [
+        "00:00", "01:00", "02:00",
+        "03:00", "04:00", "05:00",
+        "06:00", "07:00", "08:00",
+        "09:00", "10:00", "11:00",
+        "12:00", "13:00", "14:00",
+        "15:00", "16:00", "17:00",
+        "18:00", "19:00", "20:00",
+        "21:00", "22:00", "23:00"
+    ],
+    "data_format": "grib",
+    "download_format": "unarchived"
+}
+
+client = cdsapi.Client()
+client.retrieve(dataset, request).download()
+
